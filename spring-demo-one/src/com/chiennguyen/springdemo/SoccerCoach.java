f5fac9143ada2ff17d920392932fe1f81aa8e60c
nguyen the chien <the-chien.nguyen@ekino.com>
2020-11-11 18:08:15 +0700

    spring-video-28

---
+++ spring-demo-one/src/com/chiennguyen/springdemo/SoccerCoach.java
@@ -0,0 +1,11 @@
+package com.chiennguyen.springdemo;
+
+public class SoccerCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+
+		return "Shoot into goal 100 times a day!";
+	}
+
+}
